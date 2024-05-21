c9c8c90f732cd0e2385c0187c533ecb94e8de09d
salemjoban1 <61268909+salemjoban1@users.noreply.github.com>
2024-05-21 21:15:24 +0300

    convert page content to english

--- src/App.vue
+++ src/App.vue
@@ -124,26 +124,26 @@ export default({
     }
     this.headers=
       {
-        'about':{'title':'من نحن',
+        'about':{'title':'About us',
                 'color':'$primeColor'},
-        'services':{'title':'خدماتنا',
+        'services':{'title':'Services',
                 'color':'$primeColor'},
-        'works':{'title':'أعمالنا',
+        'works':{'title':'Our Work',
                 'color':'$primeColor'},
-        'clients':{'title':'عملاؤنا',
+        'clients':{'title':'Our Clients',
                 'color':'$primeColor'},
-        'contact':{'title':'تواصل معنا',
+        'contact':{'title':'Contact us',
                 'color':'#fff'},
       },
     this.slides=[
         {
           'id':'1',
           'imgsrc':'01',
-          'header':`مرحبًا بك في شركة Smart Cut للإعلان والتسويق`,
-          'subHeader':`ننتج أفضل الأعمال، بجودة وحرفيّة عالية`,
+          'header':`Welcome To Smart Cut For Adversting & Markiting`,
+          'subHeader':`We provide innovative media solution`,
           'hasBtn':true,
           'hasText':true,
-          'btn':`تواصل معنا`
+          'btn':`Contact Us`
         },
         {
           'id':'2',
@@ -174,126 +174,135 @@ export default({
         },
       ],
     this.aboutText=
-            {mainText:`الحياة لا تتوقف عن منح الفرص، لكنها حتمًا لمن ينتهزها.
-من سلطنةِ عُمان، أرض الخير والحضارة والفرص الواعدة، أسسنا نشاطنا التجاري في السابع عشر من سبتمبر عام ٢٠٢١م في مجال الترويج والإعلان، ليكون امتدادًا لرحلتنا التي قاربت ١٥ عامًا في مجال الإعلان والإنتاج الإعلامي.
-خلال رحلتنا كوّنا تجربةً مردّها حب المعرفة والاطلاع، وخبرةً منبعها العمل في مختلف البقاع والأصقاع، ثم وضعنا خلاصة خبراتنا في مشاريع تعبّر عن رؤيتنا وأهدافنا.
-في نوفمبر عام ٢٠١٣م انطلقنا بمشروعنا الأول في اليمن "شركة الرشيد الدوليّة" للإنتاج الإعلامي، لتليها بأعوامٍ قليلة انطلاقةُ شركتنا الخاصّة "Smart Cut" للإنتاج الفني في أغسطس عام ٢٠١٨م.
-جمعنا فيها طاقمًا محترفًا طموحًا، فأرسينا دعائم الثقة مع عملائنا، واكتسبنا سمعة طيبة لالتزامنا الجديّة، الاحترافيّة، المرونة في أشد الضغوط، والدقّة في المواعيد وتسليم الأعمال. `,
-                 textMore:`ولأنّ هدفنا منذ البداية كان منصبّاً على الإلهام والتأثير، واستشراف الفرص المستقبليّة للتوسّع، وتجاوز الحدود الجغرافيّة، ها نحنُ نجدّد انطلاقتنا في بلدنا الثاني عُمان، ونطمح أن تكون وجهةً وواجهةً لعلاقات قويّة ونوعيّة مع أكبر الشركات والمؤسسات.
-مع كل بصمة عمل، ولبنة نجاح، نزداد يقينًا بأن شركتنا هي شراكاتنا التي لا تنحصر بمكان وزمن، ومكسبنا هو علاقاتنا وأعمالنا التي لا تقدر بثمن، ودافعنا للاستمرار هو إيماننا بما نقدّم، وشغفنا فيما ينمّي الإنسان ويخدم. `
-                }
+      {
+        mainText:`
+Life never ceases granting opportunities to whomever takes advantage of them.
+We have started the journey of media production work 17 years ago.
+Throughout this journey, we have formed an experience based on passion for learning and exploration which sprang from working in a variety of countries. Then, we have placed the essence of our experience in projects that reflect our goals and visions.
+We have gathered an ambitious professional team, established the foundations of trust with our clients, and gained a good reputation for our commitment, professionalism, flexibility under pressures, punctuality in projects delivery.
+Right from the start, our goal focused on inspiration, influence, foreseeing future opportunities to expand, and transcending geographical borders. Therefore, we aspire to be a destination and interface for strong and unique relations with the largest companies and institutions.`,
+      textMore:`
+With every footprint we make, and every sign of success, we become more certain that our true company is our partnerships, which are not limited by time and place. Our profit is our priceless relationships and projects. Our faith and our passion for human development is what fuels our progress.`
+    }
     this.services=[
       {
         'id':1,
         'classType':'col-6 col-md-3',
-        'name':"الأفلام الترويجية والتعريفية",
+        'name':"Promotional and Corporate Films:",
         'imgSrc':'01.png',
-        'altText':'رسم يدل على مشاهدةالافلام التعريفية',
-        'description':`سواءً كانت لديك (شركة تجاريّة/ مشاريع عقاريّة/ بنوك/ مرافق تعليميّة، ثقافيّة، صحيّة، سياحيّة) نُنتج لكَ أفلامًا تستعرضُ أعمالك وإنجازاتك بأعلى مستويات الجودة والاحترافيّة، لتستقطب شريحةً أكبر من المستهدفين، وتحسّن صورتك الذهنية لديهم، وتجعل اسمك دائمًا في الصدارة.`,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':10,
-        'classType':'col-6 col-md-3',
-        'name':"الإعلانات التجارية",
-        'imgSrc':'10.png',
-        'altText':'رسم يدل على الإعلانات التجارية',
-        'description':`لاشك وأنّ الانطباع الأول لدى عميلك هو الأهم، فهو ما يبقى في ذهنه مطوّلاً، ويمنحه الثقة بمنتجك أو خدمتك، ولهذا تتنافس الجهات لجعل إعلاناتها فريدةً وجذّابة. أياً تكن رسالتك الإعلانيّة (تسويق فكرة/ منتج/ خدمة جديدة أو سابقة) نضمن لك إيصالها بطرق فعّالة، وقوالب مختلفة وشيّقة لنضاعف أرقام مبيعاتك، ونرسّخ علامتك التجاريّة. `,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':9,
-        'classType':'col-6 col-md-3',
-        'name':"الحملات الإعلانية ",
-        'imgSrc':'09.png',
-        'altText':'رسم يدل على الحملات الإعلانية',
-        'description':`يقوم خبراء التسويق في فريقنا بإعداد الخطط التسويقيّة لحملاتك الإعلانيّة المموّلة، واستخراج التقارير الكاملة عنها، مع إمكانيّة استهداف عملاء في مناطق جغرافيّة محددة أو جنسيات محددة.
-فضلاً عن تقديم الاستشارات الإعلانية عن الحملات الإعلانيّة الجاذبة للعملاء، لتوسيع رقعة نشاطك، واستقطاب عملائك، وزيادة مبيعاتك.  `,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':13,
-        'classType':'col-6 col-md-3',
-        'name':"التصميم الجرافيكي",
-        'imgSrc':'13.png',
-        'altText':'رسم يدل على التصميم الجرافيكي',
-        'description':`لأنّ التصاميم المبتكرة تضفي الروح والجاذبيّة لأي عمل، فإننا نقدم الخدمات الأفضل في: الهوية البصرية، الانفوجرافيك، الموشن جرافيك، التصميم ثلاثي الأبعاد والمتحرك.`,
+        'altText':'Promotional and Corporate image',
+        'description':`Whether you have (Trade Company, Real Estate, Oil and gas, Bank, Educational, Cultural, Health, or Tourist Facility etc.), we produce films that present your work and achievements at the highest levels of quality and professionality, to attract the widest range of your target clients, improve your brand awareness, and make your name always in the forefront.`,
         'descriptionWindow':false,
         'arrowVisible':false
       },
       {
         'id':2,
         'classType':'col-6 col-md-3',
-        'name':"الأفلام الوثائقية",
-        'altText':'رسم يدل على الافلام الوثائقية',
-        'imgSrc':'02.png',
-        'description':`ما يصنع قيمة اللحظات المميزة، هو رصيدها من الخطوات والإنجازات.
-سواءً كان لديك مشروع استثنائي تود توثيق مراحله، أو تود العودة بالزمن في مسار فيلم يستعرض تاريخ شركتك، ونجاحاتها لتعرضه في ذكرى التأسيس القادمة، أو ترغب في تسليط الضوء على قصص نجاح ملهمة في بيئة عملك. سنكون معك لنظهر ذلك بأفضل ما يكون، من الماضي إلى المستقبل. `,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':5,
-        'classType':'col-6 col-md-3',
-        'name':"التصوير الفوتوغرافي",
-        'imgSrc':'05.png',
-        'altText':'رسم يدل على التصوير',
-        'description':`لأنّ العين هي أسرع الحواس تأثرًا وتفاعلاً مع محيطها، فإننا نولي التصوير اهتمامًا بالغًا، ونوظّف أقدر المصورين وأكفأهم، كما نواكب أحدث المعدّات والتقنيات في مجال التصوير. لنجعل من كل صورة نلتقطها متعة بصرية، وتحفة فنية.`,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':15,
-        'classType':'col-6 col-md-3',
-        'name':"تصميم الشاشات",
-        'imgSrc':'15.png',
-        'altText':'رسم يدل على تصميم الشاشات',
-        'description':`لشاشات العرض أهميّة كبرى في الشركات، فهي الوسيلة الأولى وربما الأقوى في التأثير على الزوّار والعملاء المحتملين والتفاعل البصريّ معهم.
-ولهذا فإننا نُوليها اهتمامًا في خدماتنا؛ إذ ننتج لك أعمالاً متكاملة وجذّابة في تفاصيلها من كتابة المحتوى إلى التصميم والتصوير والصوت والإخراج، لتعرضها بثقة على الشاشات في مقر شركتك وفروعها، فضلاً عن إمكانيّة عرضها على شاشات الإعلانات في الشوارع، وعلى وسائل التواصل الاجتماعي، والقنوات الفضائيّة.`,
+        'name':"Commercial Ads",
+        'imgSrc':'10.png',
+        'altText':'Commercial Ads image',
+        'description': `No doubt that your clients’ first impression is the most important thing, for it gives a lasting effect in their minds and leads them to trust your services or products. Different companies compete to make unique and attractive ads. Therefore, whether the intended massage of your advertisement is promoting an idea, a product, a previous or new service, we guarantee delivering it with the most effective way to enhance your sale numbers and to consolidate your brand.`,
         'descriptionWindow':false,
         'arrowVisible':false
       },
       {
         'id':3,
         'classType':'col-6 col-md-3',
-        'name':"الفيديو كليب (فيديو غنائي)",
+        'name':"Graphic Design",
+        'imgSrc':'13.png',
+        'altText':'Graphic Design image',
+        'description':`Creative and innovative designs enliven any project and makes it attractive. Therefore, we present to you the best services in visual identity, infographics, motion graphics, 3D, and animated design.`,
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':4,
+        'classType':'col-6 col-md-3',
+        'name':"Documentary Films",
+        'altText':'Documentary Films image',
+        'imgSrc':'02.png',
+        'description':`What makes a special moment valuable is its balanced steps and achievements. Whether you have an exceptional project that you would like to document its stages, or you would like to go back in time with a documentary that explores the history of your company and its achievements to be shown on the upcoming founding anniversary, or you want to highlight the inspiring success stories at your work environment, we will help you represent it all from the past till the future in the best way possible. `,
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':5,
+        'classType':'col-6 col-md-3',
+        'name':"Photography",
+        'imgSrc':'05.png',
+        'altText':'Photography image',
+        'description': `Eyes are the fastest and most sensitive in interaction with their surroundings. Thus, we pay great attention to photography, employ the most qualified photographers, and keep up with the latest equipment and technologies in this field; making every photo we take a visual joy, and a masterpiece.`,
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':6,
+        'classType':'col-6 col-md-3',
+        'name':"Music Videos",
         'imgSrc':'03.png',
-        'altText':'رسم يدل على عمل الفيديو كليب',
-        'description':`الموسيقى لغة الشعوب المشتركة، فلا تجد طفلاً ولا بالغًا -في أي بقعة من العالم- إلا وهو يدندن نغمته المفضلة بانسجام. وحين تُدمج بالكلمات الساحرة، والمشاهد الباهرة، تصبح الكليبات الغنائيّة الوسيلة الأمثل لإيصال الرسائل الهادفة، وغرس القيم الإيجابيّة. ننتج لك كليبات غنائيّة بأعلى مستويات الاحترافيّة، بدءًا من اختيار الكلمات المؤثرة، مرورًا بمراحل الإنتاج والإخراج، لتصل رسالتك قوية، ويُصبح لأعمالك بصمة وهويّة.`,
+        'altText':'Music Videos image',
+        'description':`     Music is the common language of nations, for no man nor child in any corner of the world who do not hum their favorite tunes. Therefore, when those are combined with charming words and dazzling scenes, music videos become the perfect way to convey purposeful messages and instill positive values. We produce video clips with the highest quality starting with the choice of effective lyrics to stages of production and direction to have a strong message delivered along with your imprint and identity.`,
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':7,
+        'classType':'col-6 col-md-3',
+        'name':"Content Writing",
+        'imgSrc':'11.png',
+        'altText':'Content Writing image',
+        'description': `Words are the pillars of decisions, and the forum for ideas.
+        Our experts in content writing offer you a set of services and recommendations to express your ideas professionally: (professional drafting of your projects and business, writing your company profile or the annual report of your achievements, enriching the content of your activity’s social media platforms, creative writing in various fields and according to your vision).`,        
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':8,
+        'classType':'col-6 col-md-3',
+        'name':"TV and YouTube shows",
+        'imgSrc':'06.png',
+        'altText':'TV and YouTube shows image',
+        'description': `We offer you exclusive program ideas in various fields (education/culture/health/art and entertainment/competitions) and then produce them for you with high proficiency to deliver your message and show your brand properly.`,        
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':9,
+        'classType':'col-6 col-md-3',
+        'name':"Sound and Visual Tag",
+        'imgSrc':'08.png',
+        'altText':'Sound and Visual Tag image',
+        'description': `To enhance your logo and help you in getting into the world of innovation and competition, we present to you a logo animation service in an attractive way and producing a musical sound tag, which reflects your identity and your field of work to use it as an introduction for all your works and promotions.`,        
+        'descriptionWindow':false,
+        'arrowVisible':false
+      },
+      {
+        'id':10,
+        'classType':'col-6 col-md-3',
+        'name':"Documentation of Events",
+        'imgSrc':'04.png',
+        'altText':'Documentation of Events: image',
+        'description': `We accompany you in your important events such as conferences, seminars, and activities. We reassure you; we take care of documenting all the details and moments with photographs and videos, and with the utmost efficiency and professionalism.`,        
         'descriptionWindow':false,
         'arrowVisible':false
       },
       {
         'id':11,
         'classType':'col-6 col-md-3',
-        'name':"كتابة المحتوى",
-        'imgSrc':'11.png',
-        'altText':'رسم يدل على كتابة المحتوى',
-        'description':`الكلمة هي عماد القرار، وملتقى الأفكار.
-يقدم لك خبراؤنا في كتابة المحتوى جملةً من الخدمات والاقتراحات للتعبير عن أفكارك بشكل احترافي: (الصياغة المهنيّة لمشاريعك وأعمالك، كتابة البروفايل التعريفي لشركتك أو التقرير السنوي لإنجازاتك، إثراء محتوى منصات السوشل ميديا الخاصة بنشاطك، الكتابة الإبداعيّة في مختلف المجالات وبحسب ما تقتضيه رؤيتك).`,
+        'name':"Web Design and Development",
+        'imgSrc':'12.png',
+        'altText':'Web Design and Development image',
+        'description': `Your website is your first interface, and your virtual workplace, which is why we are keen to design and present it in a modern way to simulate your efforts on the ground.`,
         'descriptionWindow':false,
         'arrowVisible':false
       },
       {
         'id':12,
         'classType':'col-6 col-md-3',
-        'name':"تصميم وتطوير المواقع",
-        'imgSrc':'12.png',
-        'altText':'رسم يدل على تصميم وتطوير المواقع',
-        'description':`موقعك الإلكتروني هو واجهتك الأولى، ومقر عملك الافتراضي، ولهذا فإننا نحرص على تصميمه وإظهاره بشكل عصري ليحاكي جهودك على أرض الواقع.`,
-        'descriptionWindow':false,
-        'arrowVisible':false
-      },
-      {
-        'id':14,
-        'classType':'col-6 col-md-3',
-        'name':"إدارة صفحات التواصل",
+        'name':"Social Media Management:",
         'imgSrc':'14.png',
         'altText':'رسم يدل على إدارة صفحات التواصل',
-        'description':`نوظّف الإقبال الجماهيري على منصات التواصل الاجتماعي لصالحك، إذ يقوم فريقنا من كاتبي المحتوى ومصممي المنشورات ومختصي النشر وأدوات السوشل ميديا بإدارة صفحاتك الرقميّة بكفاءة، وعرض مشاريعك وخدماتك بطابع جذّاب وشيّق. `,
+        'description':`We employ the popularity of the social media to your benefit. Our team of content writers, designers, and posts' specialists in social media, manage your page in a professional manner and present your projects and services in an attractive way. `,
         'descriptionWindow':false,
         'arrowVisible':false
       },
@@ -317,12 +326,12 @@ export default({
         'iconClass':'fab fa-instagram',
         'link':'https://instagram.com/smartcut_oman'
       },
-      {
-        'id':4,
-        'name':'twitter',
-        'iconClass':'fab fa-twitter',
-        'link':'https://www.twitter.com'
-      },
+      // {
+      //   'id':4,
+      //   'name':'twitter',
+      //   'iconClass':'fab fa-twitter',
+      //   'link':'https://www.twitter.com'
+      // },
     ],
     this.directContact=[
         {
@@ -363,39 +372,39 @@ export default({
     ],
     this.clients=[
       {'id':1,
-       'name':'شركة بترومسيلة',
+       'name':'Petro Masila Company',
        'imgsrc':'01.png',
-       'altText':'شعار شركة بترومسيلة'
+       'altText':'Petro Masila logo'
        },
        {'id':2,
-       'name':'مؤسسة صلة',
+       'name':'Selah Foundation',
        'imgsrc':'03.png',
-       'altText':'شعار مؤسسة صلة'
+       'altText':'Selah logo'
        },
        {'id':3,
-       'name':'شركة بيسكو',
+       'name':'Pesco Company',
        'imgsrc':'04.png',
-       'altText':'شعار شركة بيسكو'
+       'altText':'Pesco logo'
        },
        {'id':5,
-       'name':'مؤسسة العون',
+       'name':'Al Awn Foundation',
        'imgsrc':'02.png',
-       'altText':'شعار مؤسسة العون'
+       'altText':'Al Awn logo'
        },
        {'id':6,
-       'name':'شركة الرشيد',
+       'name':'Al Rasheed',
        'imgsrc':'05.png',
-       'altText':'شعار شركة الرشيد'
+       'altText':'Al Rasheed logo'
        },
        {'id':6,
-       'name':'قناة بلقيس',
+       'name':'Palgees Channel',
        'imgsrc':'06.png',
-       'altText':'شعار قناة بلقيس'
+       'altText':'Palgess logo'
        },
        {'id':5,
-       'name':'جامعة الأندلس',
+       'name':'Al Andlas',
        'imgsrc':'07.png',
-       'altText':'شعار جامعة الأندلس'
+       'altText':'Al Andlas logo'
        },
     ],
     this.allWorks=[
